e622d8f18493c68b87a8f7f5b2d922f6bc38ce25
Josiah Swab <josiahjswab@gmail.com>
2020-01-14 13:05:11 -0800

    Seperates vanilla and react files. Adds readme.md for others. Renames foobar into scraps

--- app/main.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import FlexStyles from './components/FlexStyles'
-import RecursiveFunction from './components/RecursiveFunction'
-
-ReactDOM.render(<RecursiveFunction/>, document.getElementById("root") )
